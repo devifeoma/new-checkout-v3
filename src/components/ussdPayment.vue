bccaaa0c092a09cbde1f63978aad717cc5882767
ifeoma1 <ifeoma@flutterwavego.com>
2020-09-02 22:58:51 +0100

    Add

--- src/components/ussdPayment.vue
+++ src/components/ussdPayment.vue
@@ -1,25 +1,60 @@
 <template>
   <div>
-    <div class="transaction__details">
+    <div class="transaction__details" v-if="stage === 1">
       <div class="ussd__instruction">
         <p>
           Please choose your bank to begin payment
         </p>
       </div>
 
-      <select-bank></select-bank>
+      <div class="card input__icon">
+        <label class="label">Select Bank</label>
+        <div class="language__option">
+          <select class="select__bank">
+            <option  @click="completePayment">Click here to choose</option>
+            <option>Click here to choose</option>
+          </select>
+        </div>
+      </div>
     </div>
+
+    <div class="transaction__details" v-if="stage === 2">
+      <div class="ussd__instruction">
+        <p>
+          Dial the GTBank’s USSD code below on your mobile phone to complete the
+          payment
+        </p>
+
+        <h6>*966*6*234567*90827#</h6>
+
+        <!-- <span>Click here to copy the code</span> -->
+      </div>
+    </div>
+
+    <button type="submit" class="pay__button">
+      I have completed this payment
+    </button>
   </div>
 </template>
 
 <script>
-import selectBank from "../components/formComponents/selectBank.vue";
+// import selectBank from "../components/formComponents/selectBank.vue";
 
 export default {
   name: "ussdPayment",
   components: {
-    selectBank
-  }
+    // selectBank,
+  },
+  data: function() {
+    return {
+      stage: 1,
+    };
+  },
+  methods: {
+    completePayment() {
+      this.stage = 2;
+    },
+  },
 };
 </script>
 
