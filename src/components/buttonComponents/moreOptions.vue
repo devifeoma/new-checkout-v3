059de2e452503188789c72d04133b5e3245056a8
ifeoma1 <ifeoma@flutterwavego.com>
2020-09-04 09:04:46 +0100

    Add

--- src/components/buttonComponents/moreOptions.vue
+++ src/components/buttonComponents/moreOptions.vue
@@ -2,16 +2,16 @@
   <div class="more__payment__options">
     <p>More payment options</p>
     <svg
-      width="7"
-      height="11"
-      viewBox="0 0 7 11"
+      width="11"
+      height="7"
+      viewBox="0 0 11 7"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
-        d="M0.337763 9.99125L1.25474 10.9102L6.34961 5.77007L1.24959 0.629887L0.337763 1.54889L4.52596 5.77007L0.337763 9.99125Z"
+        d="M1.12262 0.763545L0.203613 1.68052L5.34379 6.77539L10.484 1.67537L9.56497 0.763545L5.34379 4.95175L1.12262 0.763545Z"
         fill="#666666"
       />
     </svg>
